10f59f694f560e4aa33a2a1734ffec6372b6bcfe
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2020-07-07 12:45:44 +0530

    Sample updated.

--- ListViewSample/ListViewPrism/ListViewPrism/Helper/Behavior.cs
+++ ListViewSample/ListViewPrism/ListViewPrism/Helper/Behavior.cs
@@ -8,19 +8,17 @@ namespace Grouping
 {
     class Behavior : Behavior <SfListView>
     {
-        ListViewGroupingViewModel viewModel;
         public SfListView listview { get; private set; }
         protected override void OnAttachedTo(SfListView bindable)
         {
             base.OnAttachedTo(bindable);
             listview = bindable as SfListView;
-            viewModel = new ListViewGroupingViewModel();
-            listview.BindingContext = viewModel;
             listview.ItemDragging += Listview_ItemDragging;
         }
 
         private void Listview_ItemDragging(object sender, ItemDraggingEventArgs e)
         {
+            var viewModel = (sender as SfListView).BindingContext as ListViewGroupingViewModel;
             if (e.Action == Syncfusion.ListView.XForms.DragAction.Drop)
             {
                 viewModel.isDragEndRaised = true;
